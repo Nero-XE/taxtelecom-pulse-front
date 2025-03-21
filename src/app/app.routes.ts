fa01b0be56b4e00c2255f6cbbba2e5e17dceea43
Dmitry Koshkarov <dimakoshkarov@outlook.com>
2025-03-21 17:12:06 +0500

    Добавлен роутер

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '**',
+        redirectTo: 'auth',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        loadComponent: () => import('./pages/login/login.component').then((c) => c.LoginComponent)
+    }
+];
