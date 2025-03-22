a51c0fade73b9ecbe84372b6e8bf8b017a25e426
Dmitry Koshkarov <dimakoshkarov@outlook.com>
2025-03-22 22:14:18 +0500

    Добавлены новые пути

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '**',
+        path: '',
         redirectTo: 'auth',
         pathMatch: 'full'
     },
@@ -10,5 +10,46 @@ export const routes: Routes = [
         path: 'auth',
         title: "Вход",
         loadComponent: () => import('./pages/auth/auth.component').then((c) => c.AuthComponent)
+    },
+    {
+        path: 'main',
+        loadComponent: () => import('./pages/main/main.component').then((c) => c.MainComponent),
+        children: [
+            {
+                path: '',
+                redirectTo: 'applications',
+                pathMatch: 'full'
+            },
+            {
+                path: 'search',
+                title: "Поиск",
+                loadComponent: () => import('./pages/main/pages/search/search.component').then((c) => c.SearchComponent),
+            },
+            {
+                path: 'profiles',
+                title: "Профили",
+                loadComponent: () => import('./pages/main/pages/profiles/profiles.component').then((c) => c.ProfilesComponent),
+            },
+            {
+                path: 'applications',
+                title: "Анкеты",
+                loadComponent: () => import('./pages/main/pages/applications/applications.component').then((c) => c.ApplicationsComponent),
+            },
+            {
+                path: 'moodle',
+                title: "Moodle",
+                loadComponent: () => import('./pages/main/pages/moodle/moodle.component').then((c) => c.MoodleComponent),
+            },
+            {
+                path: 'statistics',
+                title: "Статистика",
+                loadComponent: () => import('./pages/main/pages/statistics/statistics.component').then((c) => c.StatisticsComponent),
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'auth',
+        pathMatch: 'full'
     }
 ];
